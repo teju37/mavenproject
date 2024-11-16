bf239982b5ab37948e5abcf34789a44b08d43fbe
teju37 <tejugdsc@gmail.com>
2024-11-16 13:33:24 +0530

    sample maven java

---
+++ src/main/java/com/app/demo/SampleMavenJavaProject/App.java
@@ -0,0 +1,13 @@
+package com.app.demo.SampleMavenJavaProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "my name is M.Tejaswini , roll number: 22bd1a6637" );
+    }
+}
